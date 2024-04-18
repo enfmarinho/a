2af9ff07e74518d01873b2114a5581a096bf54d9
enfmarinho <eduardonestormarinho228@gmail.com>
2024-04-18 13:37:26 -0300

    initial commit.

---
+++ lib/codigo_barras.ex
@@ -0,0 +1,18 @@
+defmodule CodigoBarras do
+  @moduledoc """
+  Documentation for `CodigoBarras`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CodigoBarras.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/codigo_barras_test.exs
@@ -0,0 +1,8 @@
+defmodule CodigoBarrasTest do
+  use ExUnit.Case
+  doctest CodigoBarras
+
+  test "greets the world" do
+    assert CodigoBarras.hello() == :world
+  end
+end
