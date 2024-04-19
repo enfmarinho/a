d69a723c156b9d7d8fd1b64af5fb46ad593c5c9b
rafael <rafael.19.rosendo@gmail.com>
2024-04-19 09:21:28 -0300

    biblioteca necessária para png

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule CodigoBarras.MixProject do
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
+      {:barlix, "~> 0.6"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
