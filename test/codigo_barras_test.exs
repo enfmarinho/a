c7f21e5c7b6bc6248d2d85dfd4397f8b0ab063a3
rafael <rafael.19.rosendo@gmail.com>
2024-04-30 14:30:49 -0300

    iniciando testes

--- test/codigo_barras_test.exs
+++ test/codigo_barras_test.exs
@@ -2,7 +2,39 @@ defmodule CodigoBarrasTest do
   use ExUnit.Case
   doctest CodigoBarras
 
-  test "greets the world" do
-    assert CodigoBarras.hello() == :world
+  test "Testing the reader of the bar code " do
+    
+    input_codigo_banco = "001\n"
+    expected_codigo_banco = {:ok ,[0,0,1]}
+    assert capture_io([input: input], fn -> assert Codigobarras.Encoder.ler_codigo_banco() == expected_codigo_banco end) == input
+
+    #exemplo de teste 
+
+
+    test "Teste de códigos de barras com 44 dígitos" do
+      
+      #TODO
+    end
+
+    test "Teste de códigos de barras diferente de 44 dígitos" do
+      
+      #TODO
+    end
+
+    test "Linha digitável de 47 dígitos " do
+      
+      #TODO
+    end
+
+    test "Linha digitável diferente de 47 dígitos" do
+      
+      #TODO
+    end
+
+    test "Carregando código de barras e colocando em PNG" do
+      
+      #TODO
+    end
+
   end
 end
